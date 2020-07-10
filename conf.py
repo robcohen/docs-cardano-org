e78cd05cc648f7140e981dd7099e8d6bd0a6d2a7
Rob Cohen <rob@robcohen.dev>
2020-07-10 09:59:03 -0600

    fix

--- conf.py
+++ conf.py
@@ -78,8 +78,8 @@ html_theme = "sphinx_rtd_theme"
 html_style = 'css/modified-theme.css'
 
 html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
+    'logo_only': False,
+    'display_version': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
@@ -96,7 +96,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_logo = "cardano-logo.png"
+html_logo = ".sphinx/cardano-logo.png"
 
 html_context = {
   "display_github": True, # Add 'Edit on Github' link instead of 'View page source'
